5d2192e237573d0c46d8e6928247baa091cbf709
Dylan <442947365@qq.com>
2017-06-22 23:49:23 +0800

    修改歌词路径

--- song.js
+++ song.js
@@ -1,5 +1,5 @@
 $(function(){
-    $.get('https://github.com/SquallChen/NetEaseMusic/blob/master/lyric.json').then(function(object){
+    $.get('SquallChen/NetEaseMusic/blob/master/lyric.json').then(function(object){
         let {lyric} = object
         let array = lyric.split('\n')
         let regex =/^\[(.+)\](.*)$/
